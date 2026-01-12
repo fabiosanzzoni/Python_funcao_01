125a25430932131ccca59089f96bbe402d4515a5
Fábio Sanzzoni <168327984+fabiosanzzoni@users.noreply.github.com>
2026-01-12 17:45:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Crie uma função chamada media que receberá três números como argumentos. Esta função deve calcular a média aritmética desses três números. Para fazer isso, some os três números e, em seguida, divida o resultado por três. Por fim, a função deve retornar o valor da média aritmética calculada.
-
 def media (nota1, nota2, nota3):
     return (nota1 + nota2 + nota3)/3
 
@@ -7,5 +5,6 @@ def media (nota1, nota2, nota3):
 nota1_input = float(input("Digite a nota1"))
 nota2_input = float(input("Digite a nota2"))
 nota3_input = float(input("Digite a nota3"))
+
 
 print(f"{media(nota1_input, nota2_input, nota3_input):.1f}")
